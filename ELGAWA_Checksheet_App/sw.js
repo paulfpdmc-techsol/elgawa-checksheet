4c2c10595d24a0e8b9d864f326a183d6b8c4b41f
paulfpdmc-techsol <paul.fpdmc@gmail.com>
2026-05-11 14:55:44 -0400

    Add files via upload

--- ELGAWA_Checksheet_App/sw.js
+++ ELGAWA_Checksheet_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "elgawa-checksheet-v93";
+const CACHE_NAME = "elgawa-checksheet-v94";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 // Install — cache all assets
